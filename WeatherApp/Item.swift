7230114ceb37ff5844078a5a24f23fafbc9eac51
Shugyla <rafikovashugyla@gmail.com>
2026-02-01 18:11:54 +0500

    Initial Commit

---
+++ WeatherApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WeatherApp
+//
+//  Created by Twilight on 01.02.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Twilight on 01.02.2026.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
